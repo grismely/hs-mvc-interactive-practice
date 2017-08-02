9060824af14fd102a5d0a9847d8b330870d8a6ae
Grismely Hiraldo <ghiraldo212@gmail.com>
2017-08-02 15:15:21 +0000

    works

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,25 @@
 require 'bundler'
 Bundler.require
-
+require_relative "./models/dog.rb"
+require_relative "./models/dog.rb"
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
   end
 
+  get "/dog" do
+    @dog1 = Dog.new("Visirion","Lab",5)
+    erb :dog
+  end
+  get "/info" do
+    erb :info
+  end
+  get "/stuff" do
+    erb :stuff
+  end
+  get "/twitter" do
+    @grismely_hiraldo = User.new("GrismelyGz", "sjhaskdhs")
+    
+  end
 end
